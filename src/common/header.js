2e6f88407c7a062b0f4c25a703eb279a936318ec
ohmyskyhigh <miaorunkun@163.com>
2020-12-10 19:02:38 +0800

    header style

--- src/common/header.js
+++ src/common/header.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
-import { HeaderWrapper, Logo, Nav } from './style.js'
+import { HeaderWrapper, Logo, Nav, NavItem, NavSearch} from './style.js'
 
 class Header extends Component {
   render () {
@@ -8,7 +8,15 @@ class Header extends Component {
       <Fragment>
         <HeaderWrapper>
           <Logo/>
+          <Nav>
+            <NavItem pos='left' className='active'>首页</NavItem>
+            <NavItem pos='left'>下载</NavItem>
+            <NavItem pos='right'>写文章</NavItem>
+            <NavItem pos='right'>登录</NavItem>
+            <NavSearch placeholder='搜索' type='text'/>
+          </Nav>
         </HeaderWrapper>
+
       </Fragment>
     )
   }
--- src/common/style.js
+++ src/common/style.js
@@ -22,8 +22,40 @@ background-size: contain;
 
 export const Nav = styled.div
 `
-width: 960px;
+padding-left: 100px;
+padding-right: 100px;
+marginLeft:100px;
+marginRight:100px;
 height: 100%;
-margin: 0 auto;
-background: green
+background:green;
+`
+
+export const NavItem = styled.div
+`
+line-height:56px;
+margin-left: 25px;
+margin-right: 25px;
+font-size: 18px;
+float: ${props => props.pos};
+&.active{
+  color: #ea6f5a;
+}
+`
+
+export const NavSearch = styled.input.attrs((props) =>
+  {return ({type:props.type,
+            placeholder: props.placeholder,
+ })}
+)
+`
+width: 160px;
+height: 25px;
+margin-top: 12.5px;
+border: 0;
+border-radius: 19px;
+background: #eee;
+&::placeholder{
+  color: #999;
+  padding-left: 5px;
+}
 `
